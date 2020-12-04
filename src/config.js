38802f7de5834398a26cf496b94c7d1e7841132b
AnhTN <anhtn@rikkeisoft.com>
2020-12-04 09:14:11 +0700

    Add env var for backend URL

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@
 
 const config = Object.freeze({
 	// API_DEV: "https://api-binance-testnet.cosmostation.io/v1",
-	API_DEV: "http://10.10.10.128:5000/v1",
-	API_PROD: "https://api-binance-mainnet.cosmostation.io/v1",
+	API_DEV: process.env.REACT_APP_API_DEV || "http://localhost:5000/v1",
+	API_PROD: process.env.REACT_APP_API_PROD || "https://api-binance-mainnet.cosmostation.io/v1",
 });
 
 export default config;
