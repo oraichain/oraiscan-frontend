1a128b42002889177d63004e47b20c446e1b9297
chuyenpn <chuyenpn@rikkeisoft.com>
2020-12-25 16:36:31 +0700

    add half and max amount btn to send

---
+++ src/components/common/form-controls/input-number-format/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {useFormContext, Controller} from "react-hook-form";
+import TextField from "@material-ui/core/TextField";
+import cn from "classnames/bind";
+import NumberFormat from "react-number-format";
+
+import styles from "./index.scss";
+
+const cx = cn.bind(styles);
+
+function FormInput(props) {
+	const {control} = useFormContext();
+	const {name, placeholder, label, required, errorobj} = props;
+	let isError = false;
+	let errorMessage = "";
+	if (errorobj && errorobj.hasOwnProperty(name)) {
+		isError = true;
+		errorMessage = errorobj[name].message;
+	}
+
+	return (
+		<Controller
+			as={({onChange, value, ...rest}) => (
+				<NumberFormat customInput={TextField} thousandSeparator value={value} onValueChange={v => onChange(v.floatValue)} {...rest} />
+			)}
+			name={name}
+			control={control}
+			placeholder={placeholder || ""}
+			label={label}
+			fullWidth
+			className={cx("input-text")}
+			InputLabelProps={{
+				className: cx({"required-label": required}),
+				required: required || false,
+				shrink: true,
+			}}
+			error={isError}
+			helperText={errorMessage}
+			{...props}
+		/>
+	);
+}
+
+export default FormInput;
