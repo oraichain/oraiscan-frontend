33fbcb585b7a58b266cc2f102fcdc3723fac3559
tranquanghuy7198 <tranquanghuy7198@gmail.com>
2020-12-26 00:30:00 +0700

    feat: Get real data from API

--- src/constants/consts.js
+++ src/constants/consts.js
@@ -5,104 +5,111 @@ import _ from "lodash";
 const api = process.env.NODE_ENV === "production" ? config.API_PROD : config.API_DEV;
 
 export default Object.freeze({
-	DEFAULT_ARRAY: [],
-	API_BINANCE_DEX: "https://www.binance.org/en/trade",
-	API_BINANCE_ACCELERATED: ["https://dex-asiapacific.binance.org/api/v1", "https://dex-atlantic.binance.org/api/v1", "https://dex-european.binance.org/api/v1"],
-	BINANCE_API_ENDPOINTS: {
-		TX: tx => `/tx/${tx}?format=json`,
-	},
-	// API_COINGECKO: {
-	// 	BASE: "https://api.coingecko.com/api/v3",
-	// 	GET_MARKET_CHART_RANGE: (id = "BNB", from, to) => `/coins/${id}/market_chart/range?vs_currency=usd&from=${from}&to=${to}`,
-	// },
-	COIN_ID: "oraichain-token",
-	NUM: {
-		BASE_MULT: 100000000,
-		DEFAULT_DECIMALS: 8,
-		PAGE_SIZE: 20,
+  DEFAULT_ARRAY: [],
+  API_BINANCE_DEX: "https://www.binance.org/en/trade",
+  API_BINANCE_ACCELERATED: ["https://dex-asiapacific.binance.org/api/v1", "https://dex-atlantic.binance.org/api/v1", "https://dex-european.binance.org/api/v1"],
+  BINANCE_API_ENDPOINTS: {
+    TX: tx => `/tx/${tx}?format=json`,
+  },
+  // API_COINGECKO: {
+  // 	BASE: "https://api.coingecko.com/api/v3",
+  // 	GET_MARKET_CHART_RANGE: (id = "BNB", from, to) => `/coins/${id}/market_chart/range?vs_currency=usd&from=${from}&to=${to}`,
+  // },
+  COIN_ID: "oraichain-token",
+  NUM: {
+    BASE_MULT: 100000000,
+    DEFAULT_DECIMALS: 8,
+    PAGE_SIZE: 20,
 
-		SPARE_PAGE_CNT: 2, //  amount of pages to preload in pagination
-		BINACE_API_ROWS_LIMIT: 1000, //  max rows binance api allows
-		BINANCE_API_PAGES_LIMIT: 100, //  max page binance api allows
+    SPARE_PAGE_CNT: 2, //  amount of pages to preload in pagination
+    BINACE_API_ROWS_LIMIT: 1000, //  max rows binance api allows
+    BINANCE_API_PAGES_LIMIT: 100, //  max page binance api allows
 
-		REAL_TIME_DELAY_MS: 2000, //  real-time refetch interval(for indexedPagination)
-		DASH_REAL_TIME_DELAY_MS: 3000, //  dashboard refetch interval
-		ACCOUNT_REFETCH_INTERVAL_MS: 5000, // TODO : currently not used
-		ASSET_REFETCH_INTERVAL_MS: 80000,
-		ASSET_REFETCH_PRICE_INTERVAL_MS: 80000,
-		BASIC_DATA_FETCH_INTERVAL_MS: 30000,
-	},
-	ASSET: {
-		NAME_SEARCH_PROPERTY: ["asset", "mappedAsset", "name"],
-		ORDER_COMPARE: ["mappedAsset", "marketCap", "price", "supply"],
-	},
-	GET_LOGO_LINK: symbol =>
-		_.isString(symbol) ? `https://raw.githubusercontent.com/cosmostation/cosmostation_token_resource/master/thumnail/${_.split(symbol)[0]}}.png` : "",
-	API_BASE: api,
-	API: {
-		STATUS: "/status",
-		BLOCKLIST: "/blocks",
-		ACCOUNT: "/account",
-		ACCOUNT_TXS: acc => `/account/txs/${acc}?page=1&rows=20`,
-		TXLIST: "/txs",
-		TX: "/txs",
-		ORDERS: "/orders",
-		ASSET_IMAGES: "/assets-images?page=1&rows=1000",
-		ASSETS: "/assets?page=1&rows=1000",
-		ASSETS_BEP8: "/assets/mini-tokens?page=1&rows=1000",
-		ASSET_PRICES: "/assets?page=1&rows=1000&only_price=true",
-		ASSET: "/asset?asset=",
-		ASSET_TXS: "/assets/txs?page=1&rows=20&txAsset=",
-		ASSET_HOLDERS: "/asset-holders?&page=1&rows=20&asset=",
-		TOP_ASSETS: "/market/coin/list",
-		CHARTS: "/stats/assets/chart",
-		FEE: "/fees",
-		VALIDATORS: "/validators",
-	},
-	NETWORK: {
-		COSMOS: "cosmoshub-3",
-		IRIS: "irishub",
-		KAVA: "kava-3",
-		BINANCE: "binance",
-	},
-	PREFIX: {
-		COSMOS: "cosmos",
-		IRIS: "iris",
-		KAVA: "kava",
-	},
+    REAL_TIME_DELAY_MS: 2000, //  real-time refetch interval(for indexedPagination)
+    DASH_REAL_TIME_DELAY_MS: 3000, //  dashboard refetch interval
+    ACCOUNT_REFETCH_INTERVAL_MS: 5000, // TODO : currently not used
+    ASSET_REFETCH_INTERVAL_MS: 80000,
+    ASSET_REFETCH_PRICE_INTERVAL_MS: 80000,
+    BASIC_DATA_FETCH_INTERVAL_MS: 30000,
+  },
+  ASSET: {
+    NAME_SEARCH_PROPERTY: ["asset", "mappedAsset", "name"],
+    ORDER_COMPARE: ["mappedAsset", "marketCap", "price", "supply"],
+  },
+  GET_LOGO_LINK: symbol =>
+    _.isString(symbol) ? `https://raw.githubusercontent.com/cosmostation/cosmostation_token_resource/master/thumnail/${_.split(symbol)[0]}}.png` : "",
+  API_BASE: api,
+  API: {
+    STATUS: "/status",
+    BLOCKLIST: "/blocks",
+    ACCOUNT: "/account",
+    ACCOUNT_TXS: acc => `/account/txs/${acc}?page=1&rows=20`,
+    TXLIST: "/txs",
+    TXS_LIST: "/txs?limit=5&after=15&before=25",
+    TX: "/txs",
+    ORDERS: "/orders",
+    ASSET_IMAGES: "/assets-images?page=1&rows=1000",
+    ASSETS: "/assets?page=1&rows=1000",
+    ASSETS_BEP8: "/assets/mini-tokens?page=1&rows=1000",
+    ASSET_PRICES: "/assets?page=1&rows=1000&only_price=true",
+    ASSET: "/asset?asset=",
+    ASSET_TXS: "/assets/txs?page=1&rows=20&txAsset=",
+    ASSET_HOLDERS: "/asset-holders?&page=1&rows=20&asset=",
+    TOP_ASSETS: "/market/coin/list",
+    CHARTS: "/stats/assets/chart",
+    FEE: "/fees",
+    VALIDATORS: "/validators",
+    VALIDATOR: "/validator",
+    DELEGATOR: "/delegator",
+    PROPOSED_BLOCKS: "/proposed-blocks",
+    MISSED_BLOCKS: "/validator/missed-block"
+  },
+  NETWORK: {
+    COSMOS: "cosmoshub-3",
+    IRIS: "irishub",
+    KAVA: "kava-3",
+    BINANCE: "binance",
+  },
+  PREFIX: {
+    COSMOS: "cosmos",
+    IRIS: "iris",
+    KAVA: "kava",
+    ORAI_ADDR: "orai",
+    ORAI_OPER: "oraivaloper",
+    ORAI_CONS: "oraivalcons"
+  },
+  MINTSCAN_URL: {
+    COSMOS: "https://www.mintscan.io/",
+    KAVA: "https://kava.mintscan.io/",
+    IRIS: "https://irishub.mintscan.io/",
+  },
 
-	MINTSCAN_URL: {
-		COSMOS: "https://www.mintscan.io/",
-		KAVA: "https://kava.mintscan.io/",
-		IRIS: "https://irishub.mintscan.io/",
-	},
+  LINK: {
+    GOOGLE: "https://play.google.com/store/apps/details?id=wannabit.io.cosmostaion",
+    IOS: "https://apple.co/2IAM3Xm",
+    WEB: "https://wallet.cosmostation.io",
+    COSMOSTATION: "https://www.cosmostation.io/",
+    BINANCEDEX: "https://www.binance.org/",
+    COINGECKO_ORAI: "https://www.coingecko.com/en/coins/oraichain-token",
+  },
 
-	LINK: {
-		GOOGLE: "https://play.google.com/store/apps/details?id=wannabit.io.cosmostaion",
-		IOS: "https://apple.co/2IAM3Xm",
-		WEB: "https://wallet.cosmostation.io",
-		COSMOSTATION: "https://www.cosmostation.io/",
-		BINANCEDEX: "https://www.binance.org/",
-		COINGECKO_ORAI: "https://www.coingecko.com/en/coins/oraichain-token",
-	},
-
-	MENU: [
-		{
-			display: "DASHBOARD",
-			route: "/",
-			primary: true,
-		},
-		{
-			display: "VALIDATOR",
-			route: "/validators",
-		},
-		{
-			display: "BLOCKS",
-			route: "/blocks",
-		},
-		{
-			display: "TRANSACTIONS",
-			route: "/txs",
-		},
-	],
+  MENU: [
+    {
+      display: "DASHBOARD",
+      route: "/",
+      primary: true,
+    },
+    {
+      display: "VALIDATOR",
+      route: "/validators",
+    },
+    {
+      display: "BLOCKS",
+      route: "/blocks",
+    },
+    {
+      display: "TRANSACTIONS",
+      route: "/txs",
+    },
+  ],
 });
--- src/containers/ValidatorDetails/ValidatorDetails.js
+++ src/containers/ValidatorDetails/ValidatorDetails.js
@@ -1,59 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import IC_CHECK from "src/assets/validatorDetails/check.svg";
-import BINANCE_LOGO from "src/assets/validatorDetails/binance_logo.svg";
 import ORAI_LOGO from "src/assets/common/orai_favicon.png";
 import IC_PASTE from "src/assets/validatorDetails/paste.svg";
 import IC_BLOCKS from "src/assets/validatorDetails/blocks.svg";
-import IC_SHIRT from "src/assets/validatorDetails/shirt.svg";
 import IC_GOOD_BLOCK from "src/assets/validatorDetails/good_block.svg";
 import IC_BAD_BLOCK from "src/assets/validatorDetails/bad_block.svg";
+import Axios from "axios";
+import { getDelegators, getMissedBlocks, getProposedBlocks, getValidator } from "src/lib/api";
+import { commafy, formatTime } from "src/helpers/helper";
 
 export default function (props) {
-  let proposedBlocks = [
-    {
-      height: 209610,
-      blockhash: "608080F5...33E8E8A9",
-      txs: 0,
-      time: "5m ago"
-    },
-    {
-      height: 209610,
-      blockhash: "608080F5...33E8E8A9",
-      txs: 0,
-      time: "5m ago"
-    },
-    {
-      height: 209610,
-      blockhash: "608080F5...33E8E8A9",
-      txs: 0,
-      time: "5m ago"
-    },
-    {
-      height: 209610,
-      blockhash: "608080F5...33E8E8A9",
-      txs: 0,
-      time: "5m ago"
-    },
-    {
-      height: 209610,
-      blockhash: "608080F5...33E8E8A9",
-      txs: 0,
-      time: "5m ago"
-    },
-    {
-      height: 209610,
-      blockhash: "608080F5...33E8E8A9",
-      txs: 0,
-      time: "5m ago"
-    },
-    {
-      height: 209610,
-      blockhash: "608080F5...33E8E8A9",
-      txs: 0,
-      time: "5m ago"
-    }
-  ];
+  const [validatorDetails, setValidatorDetails] = useState({
+    accountAddress: "",
+    consensusAddress: "",
+    operatorAddress: "",
+    votingPower: "",
+    bondedHeight: 0,
+    commission: "",
+    details: "",
+    website: "",
+    proposedBlocks: [],
+    delegators: [],
+    missedBlocks: []
+  });
 
   let events = [
     {
@@ -101,45 +71,74 @@ export default function (props) {
   ];
 
   let missedBlocks = [92, 93, 94, 95, 96, 97, 98, 99];
-  let blockMatrix = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const blockMatrix = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  let delegators = [
-    {
-      address: "608080F5...33E8E8A9",
-      amount: 6784500,
-      share: 56.75
-    },
-    {
-      address: "608080F5...33E8E8A9",
-      amount: 6784500,
-      share: 56.75
-    },
-    {
-      address: "608080F5...33E8E8A9",
-      amount: 6784500,
-      share: 56.75
-    },
-    {
-      address: "608080F5...33E8E8A9",
-      amount: 6784500,
-      share: 56.75
-    },
-    {
-      address: "608080F5...33E8E8A9",
-      amount: 6784500,
-      share: 56.75
-    },
-    {
-      address: "608080F5...33E8E8A9",
-      amount: 6784500,
-      share: 56.75
-    },
-    {
-      address: "608080F5...33E8E8A9",
-      amount: 6784500,
-      share: 56.75
-    }
-  ];
+  React.useEffect(() => {
+    let info = {};
+    let validatorInfo = props.match.params.validator;
+    getValidator(validatorInfo, Axios.CancelToken.source().token)
+      .then(res => {
+        if (res.data !== null) {
+          info.accountAddress = res.data.account_address;
+          info.operatorAddress = res.data.operator_address;
+          info.consensusAddress = res.data.consensus_address;
+          info.votingPower = res.data.voting_power;
+          info.bondedHeight = res.data.bond_height;
+          info.commission = res.data.commission_rate;
+          info.details = res.data.description.details;
+          info.website = res.data.description.website;
+        }
+      })
+      .then(() => getDelegators(validatorInfo, 1, Axios.CancelToken.source().token))
+      .then(res => {
+        if (res.data.data !== null) {
+          let arrDelegators = [];
+          res.data.data.forEach(delegator => {
+            arrDelegators.push({
+              address: delegator.delegator_address,
+              amount: delegator.amount,
+              share: delegator.shares
+            });
+          });
+          info.delegators = arrDelegators;
+        }
+      })
+      .then(() => getProposedBlocks(validatorInfo, 1, Axios.CancelToken.source().token))
+      .then(res => {
+        if (res.data.data !== null) {
+          let arrProposedBlocks = [];
+          res.data.data.forEach(block => {
+            arrProposedBlocks.push({
+              height: block.height,
+              blockhash: block.block_hash,
+              txs: block.total_txs,
+              time: block.timestamp
+            });
+          });
+          info.proposedBlocks = arrProposedBlocks;
+        }
+      })
+      .then(() => getMissedBlocks(validatorInfo, Axios.CancelToken.source().token))
+      .then(res => {
+        // Process missed blocks here
+      })
+      .then(() => {
+        console.log("Info", info);
+        setValidatorDetails({
+          accountAddress: info.accountAddress,
+          consensusAddress: info.consensusAddress,
+          operatorAddress: info.operatorAddress,
+          votingPower: info.votingPower,
+          bondedHeight: info.bondedHeight,
+          commission: info.commission,
+          details: info.details,
+          website: info.website,
+          proposedBlocks: info.proposedBlocks,
+          delegators: info.delegators,
+          missedBlocks: missedBlocks
+        });
+      });
+  }, []);
 
   return (
     <div style={{ width: "100%" }}>
@@ -181,31 +180,31 @@ export default function (props) {
               <div style={{ fontSize: 14, fontWeight: "bold", marginRight: 5 }}>Operator address</div>
               <img src={IC_PASTE} />
             </div>
-            <div style={{ fontSize: 12, marginBottom: 10 }}>cosmosvaloper</div>
+            <div style={{ fontSize: 12, marginBottom: 10 }}>{validatorDetails.operatorAddress === "" ? "--" : validatorDetails.operatorAddress}</div>
             <div style={{ fontSize: 14, fontWeight: "bold", marginBottom: 4 }}>Address</div>
-            <div style={{ fontSize: 12, color: "#1B57F0" }}>cosmos</div>
+            <div style={{ fontSize: 12, color: "#1B57F0" }}>{validatorDetails.accountAddress === "" ? "--" : validatorDetails.accountAddress}</div>
           </div>
           <div style={{ ...styles.card, width: "65%" }}>
             <div style={{ display: "flex", flexFlow: "row" }}>
               <div style={{ flex: 6 }}>
                 <div style={{ fontSize: 12, fontWeight: "bold" }}>Website</div>
-                <div style={{ fontSize: 12, color: "#1B57F0" }}>https://orai.com</div>
+                <div style={{ fontSize: 12, color: "#1B57F0" }}>{validatorDetails.website === "" ? "--" : validatorDetails.website}</div>
                 <div style={{ fontSize: 12, fontWeight: "bold", marginTop: 15 }}>Commission</div>
-                <div style={{ fontSize: 12 }}>2.50%</div>
+                <div style={{ fontSize: 12 }}>{Number((parseFloat(validatorDetails.commission) * 100).toFixed(2))}%</div>
                 <div style={{ fontSize: 12, fontWeight: "bold", marginTop: 15 }}>Uptime</div>
                 <div style={{ fontSize: 12 }}>100%</div>
               </div>
               <div style={{ flex: 7 }}>
                 <div style={{ fontSize: 12, fontWeight: "bold" }}>Voting power</div>
-                <div style={{ fontSize: 12 }}>6.56% (12,394,000 ORAI)</div>
+                <div style={{ fontSize: 12 }}>6.56% ({commafy(validatorDetails.votingPower)} ORAI)</div>
                 <div style={{ fontSize: 12, fontWeight: "bold", marginTop: 15 }}>Bonded Height</div>
-                <div style={{ fontSize: 12 }}>--</div>
+                <div style={{ fontSize: 12 }}>{validatorDetails.bondedHeight}</div>
                 <div style={{ fontSize: 12, fontWeight: "bold", marginTop: 15 }}>Self Bonded</div>
                 <div style={{ fontSize: 12 }}>-- ORAI (--%)</div>
               </div>
               <div style={{ flex: 8 }}>
                 <div style={{ fontSize: 12, fontWeight: "bold" }}>Details</div>
-                <div style={{ fontSize: 12 }}>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </div>
+                <div style={{ fontSize: 12 }}>{validatorDetails.details === "" ? "--" : validatorDetails.details}</div>
                 <div style={{ fontSize: 12, color: "#1B57F0", marginTop: 15 }}>Show more</div>
               </div>
             </div>
@@ -236,20 +235,20 @@ export default function (props) {
                   <div style={{ fontSize: 12, fontWeight: "bold", color: "#1B57F0" }}>Time</div>
                 </div>
               </div>
-              {proposedBlocks.map((block, index) => (
-                <div style={{ display: "flex", flexFlow: "row", backgroundColor: index % 2 === 1 ? "#F9F9F9" : "#FFF", borderBottomLeftRadius: index === proposedBlocks.length - 1 ? 5 : 0, borderBottomRightRadius: index === proposedBlocks.length - 1 ? 5 : 0 }} key={index}>
+              {validatorDetails.proposedBlocks.map((block, index) => (
+                <div style={{ display: "flex", flexFlow: "row", backgroundColor: index % 2 === 1 ? "#F9F9F9" : "#FFF", borderBottomLeftRadius: index === validatorDetails.proposedBlocks.length - 1 ? 5 : 0, borderBottomRightRadius: index === validatorDetails.proposedBlocks.length - 1 ? 5 : 0 }} key={index}>
                   <div style={{ flex: 14, padding: 10, display: "flex", flexDirection: "column", alignItems: "flex-end" }}>
                     <div style={{ fontSize: 12, color: "#1B57F0" }}>{block.height}</div>
                   </div>
                   {/* <div style={{ width: 1, height: 30, backgroundColor: "#E7E7E7" }} /> */}
                   <div style={{ flex: 33, padding: 10 }}>
-                    <div style={{ fontSize: 12, color: "#1B57F0" }}>{block.blockhash}</div>
+                    <div style={{ fontSize: 12, color: "#1B57F0" }}>{block.blockhash.slice(0, 10)}...{block.blockhash.slice(block.blockhash.length - 10, block.blockhash.length)}</div>
                   </div>
                   <div style={{ flex: 18, padding: 10, display: "flex", flexDirection: "column", alignItems: "flex-end" }}>
                     <div style={{ fontSize: 12 }}>{block.txs}</div>
                   </div>
                   <div style={{ flex: 18, padding: 10, display: "flex", flexDirection: "column", alignItems: "flex-end" }}>
-                    <div style={{ fontSize: 12 }}>{block.time}</div>
+                    <div style={{ fontSize: 12 }}>{formatTime(block.time)}</div>
                   </div>
                 </div>
               ))}
@@ -267,7 +266,7 @@ export default function (props) {
               {blockMatrix.map((item, rowIndex) => (
                 <div key={rowIndex}>
                   {blockMatrix.map((item, colIndex) => (
-                    <img src={missedBlocks.indexOf(rowIndex * 10 + colIndex) > 0 ? IC_BAD_BLOCK : IC_GOOD_BLOCK} style={{ marginLeft: 6 }} />
+                    <img src={validatorDetails.missedBlocks.indexOf(rowIndex * 10 + colIndex) > 0 ? IC_BAD_BLOCK : IC_GOOD_BLOCK} style={{ marginLeft: 6 }} />
                   ))}
                 </div>
               ))}
@@ -289,18 +288,18 @@ export default function (props) {
                 <div style={{ fontSize: 12, fontWeight: "bold", color: "#1B57F0" }}>Share</div>
               </div>
             </div>
-            {delegators.map((delegator, index) => (
-              <div style={{ display: "flex", flexFlow: "row", backgroundColor: index % 2 === 1 ? "#F9F9F9" : "#FFF", borderBottomLeftRadius: index === proposedBlocks.length - 1 ? 5 : 0, borderBottomRightRadius: index === proposedBlocks.length - 1 ? 5 : 0 }} key={index}>
+            {validatorDetails.delegators.map((delegator, index) => (
+              <div style={{ display: "flex", flexFlow: "row", backgroundColor: index % 2 === 1 ? "#F9F9F9" : "#FFF", borderBottomLeftRadius: index === validatorDetails.delegators.length - 1 ? 5 : 0, borderBottomRightRadius: index === validatorDetails.delegators.length - 1 ? 5 : 0 }} key={index}>
                 {/* <div style={{ width: 1, height: 30, backgroundColor: "#E7E7E7" }} /> */}
                 <div style={{ flex: 39, padding: 10 }}>
-                  <div style={{ fontSize: 12, color: "#1B57F0" }}>{delegator.address}</div>
+                  <div style={{ fontSize: 12, color: "#1B57F0" }}>{delegator.address.slice(0, 14)}...{delegator.address.slice(delegator.address.length - 10, delegator.address.length)}</div>
                 </div>
                 <div style={{ flex: 26, padding: 10, display: "flex", flexDirection: "row", justifyContent: "flex-end" }}>
-                  <div style={{ fontSize: 12 }}>{delegator.amount}</div>
+                  <div style={{ fontSize: 12 }}>{commafy(delegator.amount)}</div>
                   <div style={{ fontSize: 12, color: "#5F51FF", marginLeft: 6 }}>ORAI</div>
                 </div>
                 <div style={{ flex: 18, padding: 10, display: "flex", flexDirection: "column", alignItems: "flex-end" }}>
-                  <div style={{ fontSize: 12 }}>{delegator.share}</div>
+                  <div style={{ fontSize: 12 }}>{Number(delegator.share / delegator.amount * 100).toFixed(2)}%</div>
                 </div>
               </div>
             ))}
