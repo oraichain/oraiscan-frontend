73f9f07f9f44da38f8fc87357125bf281ac19c80
ThinhDT-Rikkeisoft <thinhdt@rikkeisoft.com>
2021-01-04 10:03:01 +0700

    feature/change block list ui

--- src/components/TxList/TransactionTable/TransactionTable.js
+++ src/components/TxList/TransactionTable/TransactionTable.js
@@ -10,17 +10,16 @@ import styles from "./TransactionTable.scss";
 import successIcon from "src/assets/transactions/success_ic.svg";
 import failureIcon from "src/assets/transactions/fail_ic.svg";
 import moreIcon from "src/assets/transactions/tx_more_btn.svg";
-import Skeleton from "react-skeleton-loader";
 
 const TransactionTable = memo(({data = []}) => {
 	const cx = classNames.bind(styles);
-	const txHashHeaderCell = <div className={cx("tx-hash-header-cell")}>TxHash</div>;
-	const typeHeaderCell = <div className={cx("type-header-cell")}>Type</div>;
-	const resultHeaderCell = <div className={cx("result-header-cell")}>Result</div>;
-	const amountHeaderCell = <div className={cx("amount-header-cell")}>Amount</div>;
-	const feeHeaderCell = <div className={cx("fee-header-cell")}>Fee</div>;
-	const heightHeaderCell = <div className={cx("height-header-cell")}>Height</div>;
-	const timeHeaderCell = <div className={cx("time-header-cell")}>Time</div>;
+	const txHashHeaderCell = <div className={cx("header-cell", "align-left")}>TxHash</div>;
+	const typeHeaderCell = <div className={cx("header-cell", "align-left")}>Type</div>;
+	const resultHeaderCell = <div className={cx("header-cell", "align-center")}>Result</div>;
+	const amountHeaderCell = <div className={cx("header-cell", "align-right")}>Amount</div>;
+	const feeHeaderCell = <div className={cx("header-cell", "align-right")}>Fee</div>;
+	const heightHeaderCell = <div className={cx("header-cell", "align-right")}>Height</div>;
+	const timeHeaderCell = <div className={cx("header-cell", "align-right")}>Time</div>;
 	const headerCells = [txHashHeaderCell, typeHeaderCell, resultHeaderCell, amountHeaderCell, feeHeaderCell, heightHeaderCell, timeHeaderCell];
 	const headerCellStyles = [
 		{minWidth: "350px"}, // Tx hash header cell
@@ -31,16 +30,12 @@ const TransactionTable = memo(({data = []}) => {
 		}
 
 		return data.map(item => {
-			const txHashDataCell = item.tx_hash ? (
-				_.isNil(item?.tx_hash) ? (
-					<div className={cx("align-left")}>-</div>
-				) : (
-					<NavLink className={cx("tx-hash-data-cell")} to={`${consts.API.TXLIST}/${item.tx_hash}`}>
-						{reduceString(item.tx_hash, 6, 6)}
-					</NavLink>
-				)
+			const txHashDataCell = _.isNil(item?.tx_hash) ? (
+				<div className={cx("align-left")}>-</div>
 			) : (
-				<Skeleton />
+				<NavLink className={cx("tx-hash-data-cell", "align-left")} to={`${consts.API.TXLIST}/${item.tx_hash}`}>
+					{reduceString(item.tx_hash, 6, 6)}
+				</NavLink>
 			);
 
 			const typeDataCell = _.isNil(item?.messages?.[0]?.type) ? (
@@ -87,23 +82,23 @@ const TransactionTable = memo(({data = []}) => {
 				_.isNil(item?.fee?.amount?.[0]?.amount) || _.isNil(item?.fee?.amount?.[0]?.denom) ? (
 					<div className={cx("align-right")}>-</div>
 				) : (
-					<div className={cx("fee-data-cell")}>
+					<div className={cx("fee-data-cell", "align-right")}>
 						<span>{formatOrai(item.fee.amount[0].amount)}</span>
-						<span className={cx("blueColor", "uppercase")}>{item.fee.amount[0].denom}</span>
+						<span className={cx("uppercase")}>{item.fee.amount[0].denom}</span>
 					</div>
 				);
 
 			const heightDataCell = _.isNil(item?.height) ? (
 				<div className={cx("align-right")}>-</div>
 			) : (
-				<NavLink className={cx("height-data-cell")} to={`${consts.API.BLOCKLIST}/${item.height}`}>
+				<NavLink className={cx("height-data-cell", "align-right")} to={`${consts.API.BLOCKLIST}/${item.height}`}>
 					{item.height}
 				</NavLink>
 			);
 			const timeDataCell = _.isNil(item?.timestamp) ? (
 				<div className={cx("align-right")}>-</div>
 			) : (
-				<div className={cx("time-data-cell")}>{setAgoTime(item.timestamp)}</div>
+				<div className={cx("time-data-cell", "align-right")}>{setAgoTime(item.timestamp)}</div>
 			);
 			return [txHashDataCell, typeDataCell, resultDataCell, amountDataCell, feeDataCell, heightDataCell, timeDataCell];
 		});
