8358929dcfc2838b531d7cfc30960b53c377d173
Ly Trung Kien <lytrungkientq99@gmail.com>
2021-01-15 10:11:02 +0700

    page wallet coming soon

--- src/components/Wallet/DelegatedValidator/index.js
+++ src/components/Wallet/DelegatedValidator/index.js
@@ -1,3 +1,16 @@
-import DelegatedValidator from "./DelegatedValidator";
+// import DelegatedValidator from "./DelegatedValidator";
 
+// export default DelegatedValidator;
+
+import React from "react";
+const DelegatedValidator = () => {
+	return (
+		<div
+			style={{
+				height: "300px",
+			}}>
+			<p style={{fontSize: "28px", fontWeight: "500"}}>Coming soon</p>
+		</div>
+	);
+};
 export default DelegatedValidator;
--- src/components/Wallet/Transaction/index.js
+++ src/components/Wallet/Transaction/index.js
@@ -1,3 +1,15 @@
-import Transaction from "./Transaction";
+// import Transaction from "./Transaction";
 
+// export default Transaction;
+import React from "react";
+const Transaction = () => {
+	return (
+		<div
+			style={{
+				height: "300px",
+			}}>
+			<p style={{fontSize: "28px", fontWeight: "500"}}>Coming soon</p>
+		</div>
+	);
+};
 export default Transaction;
