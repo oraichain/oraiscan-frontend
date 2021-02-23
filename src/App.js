bcdafccb14226bb9f97d15fd95c20335e63c8d82
chuyenpn <chuyenpn@rikkeisoft.com>
2021-02-23 22:18:41 +0700

    add count down on 2021-02-24

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import classNames from "classnames/bind";
 import {useTheme} from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Container from "@material-ui/core/Container";
+import Countdown from "react-countdown";
 import Router from "src/containers/Router/Router";
 import Header from "src/containers/Header";
 import SearchArea from "src/components/Dashboard/SearchArea";
@@ -11,6 +12,7 @@ import Tabs from "src/containers/Tabs";
 import Footer from "src/containers/Footer";
 import Alert from "src/components/common/Alert/Alert";
 import styles from "./App.scss";
+import CustomCountDown from "./CustomCountDown";
 
 const cx = classNames.bind(styles);
 
@@ -18,18 +20,45 @@ export default function() {
 	const theme = useTheme();
 	const isLargeScreen = useMediaQuery(theme.breakpoints.up("lg"));
 	const {openPageBar} = useSelector(state => state.global);
-	return (
-		<div className={cx("app")}>
-			<Alert />
-			<Header />
-			{isLargeScreen && (
-				<Container>
-					<SearchArea />
-				</Container>
-			)}
-			{(isLargeScreen || openPageBar) && <Tabs />}
-			<Router />
-			<Footer />
-		</div>
-	);
+
+	const renderer = ({completed, ...rest}) => {
+		if (completed) {
+			return (
+				<div className={cx("app")}>
+					<Alert />
+					<Header />
+					{isLargeScreen && (
+						<Container>
+							<SearchArea />
+						</Container>
+					)}
+					{(isLargeScreen || openPageBar) && <Tabs />}
+					<Router />
+					<Footer />
+				</div>
+			);
+		}
+
+		return <CustomCountDown {...rest} />;
+	};
+
+	const releaseDate = Date.parse("2021-02-24T20:00:00.000+07:00");
+	// const releaseDate = Date.parse('2021-02-23T19:08:00.000+07:00');
+
+	return <Countdown date={releaseDate} renderer={renderer} />;
+
+	// return (
+	// 	<div className={cx("app")}>
+	// 		<Alert />
+	// 		<Header />
+	// 		{isLargeScreen && (
+	// 			<Container>
+	// 				<SearchArea />
+	// 			</Container>
+	// 		)}
+	// 		{(isLargeScreen || openPageBar) && <Tabs />}
+	// 		<Router />
+	// 		<Footer />
+	// 	</div>
+	// );
 }
---
+++ src/CustomCountDown.js
@@ -0,0 +1,65 @@
+import React, {useState, useEffect} from "react";
+import classNames from "classnames/bind";
+import logo from "src/assets/header/logo.svg";
+
+import styles from "./App.scss";
+
+const cx = classNames.bind(styles);
+
+export default function({days, hours, minutes, seconds}) {
+	const [state, setState] = useState({
+		width: "100%",
+		height: "100%",
+	});
+
+	useEffect(() => {
+		setState({
+			width: window.innerWidth - 20 + "px",
+			height: window.innerHeight - 0 + "px",
+		});
+	}, []);
+
+	return (
+		<div className={cx("countdown")} style={state}>
+			<div className={cx("menu")}>
+				<div className={cx("menu-left")}>
+					<div>
+						{" "}
+						<img src={logo} alt={"logo"} className={cx("logo")} />{" "}
+					</div>
+					<div className={cx("name")}>Oraiscan</div>
+				</div>
+				<div className={cx("menu-right")}>
+					<div>
+						{" "}
+						<a href='/'> Home </a>{" "}
+					</div>
+					<div>
+						{" "}
+						<a href='https://liquidity.orai.io/'> Product </a>{" "}
+					</div>
+					<div>
+						{" "}
+						<a href='https://orai.io/Tokenomics'> Tokenomic </a>{" "}
+					</div>
+					<div>
+						{" "}
+						<a href='https://orai.io/#team'> About </a>{" "}
+					</div>
+					<div>
+						{" "}
+						<a href='https://docs.orai.io/docs/whitepaper/introduction/'> Whitepaper </a>{" "}
+					</div>
+				</div>
+			</div>
+			<div className={cx("title")}> Mainnet Launch on ORAISCAN </div>
+			<div className={cx("remain")}>
+				{days} DAY {hours > 9 ? hours : "0" + hours}:{minutes > 9 ? minutes : "0" + minutes}:{seconds > 9 ? seconds : "0" + seconds}
+			</div>
+			<div className={cx("join-us")}>
+				<div> Join our journey to create a better world </div>
+				<div> Using the power of Artificial Intelligence interconnected with Blockchain </div>
+			</div>
+		</div>
+	);
+}
