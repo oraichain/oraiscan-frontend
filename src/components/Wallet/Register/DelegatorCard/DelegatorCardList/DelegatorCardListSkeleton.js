d999b887e6987f10efa85ad587f52237fdc7d023
ThinhDT-Rikkeisoft <thinhdt@rikkeisoft.com>
2021-03-03 10:46:48 +0700

    feature/complete orai wallet change validator info screen

---
+++ src/components/Wallet/Register/DelegatorCard/DelegatorCardList/DelegatorCardListSkeleton.js
@@ -0,0 +1,55 @@
+import React, {memo} from "react";
+import Skeleton from "react-loading-skeleton";
+import classNames from "classnames/bind";
+import styles from "./DelegatorCardList.scss";
+
+const cx = classNames.bind(styles);
+
+const DelegatorCardListSkeleton = memo(({rows = 5}) => {
+	let delegatorCardListItems = [];
+	for (let i = 1; i <= rows; i++) {
+		delegatorCardListItems.push(
+			<div className={cx("delegator-card-list-item")} key={"delegator-card-list-item-" + i}>
+				<table>
+					<tbody>
+						<tr>
+							<td>
+								<div className={cx("item-title")}>Address</div>
+							</td>
+							<td>
+								<div className={cx("item-text")}>
+									<Skeleton />
+								</div>
+							</td>
+						</tr>
+
+						<tr>
+							<td>
+								<div className={cx("item-title")}>Staked (ORAI)</div>
+							</td>
+							<td>
+								<div className={cx("item-title")}>Claimable Reward (ORAI)</div>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<div className={cx("item-text")}>
+									<Skeleton />
+								</div>
+							</td>
+							<td>
+								<div className={cx("item-text")}>
+									<Skeleton />
+								</div>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		);
+	}
+
+	return <div className='delegator-card-list'>{delegatorCardListItems}</div>;
+});
+
+export default DelegatorCardListSkeleton;
