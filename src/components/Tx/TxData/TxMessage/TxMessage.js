cfaf49e425dcaec900644fb21c8eee0d432d2618
chuyenbka <chuyenbka@gmail.com>
2021-05-17 15:53:30 +0700

    change msg type from server

--- src/components/Tx/TxData/TxMessage/TxMessage.js
+++ src/components/Tx/TxData/TxMessage/TxMessage.js
@@ -23,9 +23,13 @@ const TxMessage = ({msg, data}) => {
 	const fees = useSelector(state => state.blockchain.fees);
 	const status = useSelector(state => state.blockchain.status);
 	const storageData = useSelector(state => state.contact);
-	console.log(msg);
-	const {value} = msg;
-	const type = msg["@type"];
+	const value = msg;
+	// console.log(msg)
+	let type = msg["@type"];
+	if (type.indexOf(".") > -1) {
+		const typeArr = type.split(".");
+		type = "cosmos-sdk/" + typeArr[typeArr.length - 1];
+	}
 	const {memo} = data;
 
 	const messageDetails = useMemo(() => {
