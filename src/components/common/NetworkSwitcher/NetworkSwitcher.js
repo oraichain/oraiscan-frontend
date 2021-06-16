639967c96688e146199a4de0b2d14b86ce5d31db
thinhdt1994 <thinhdt@rikkeisoft.com>
2021-06-16 15:19:31 +0700

    feature/change ui of network dropdown

--- src/components/common/NetworkSwitcher/NetworkSwitcher.js
+++ src/components/common/NetworkSwitcher/NetworkSwitcher.js
@@ -1,11 +1,15 @@
-import React, {memo} from "react";
+import React, {memo, useState, useEffect, useRef} from "react";
 import {networks} from "src/constants/networks";
+import _ from "lodash";
 import classNames from "classnames/bind";
 import styles from "./NetworkSwitcher.scss";
+import DownAngleIcon from "src/icons/DownAngleIcon";
 
 const cx = classNames.bind(styles);
 
 const NetworkSwitcher = memo(({}) => {
+	const selectedItemRef = useRef(null);
+	const listRef = useRef(null);
 	let network = networks.MAINNET;
 	let setNetwork = _ => {};
 
@@ -14,23 +18,64 @@ const NetworkSwitcher = memo(({}) => {
 
 		setNetwork = network => {
 			localStorage.setItem("network", network);
+			window.location.reload();
 		};
 	}
 
+	const showList = () => {
+		if (!_.isNil(listRef.current)) {
+			listRef.current.style.display = "block";
+		}
+	};
+
+	const hideList = () => {
+		if (!_.isNil(listRef.current)) {
+			listRef.current.style.display = "none";
+		}
+	};
+
+	const clickListener = event => {
+		if (event.target.hasAttribute("data-network")) {
+			setNetwork(event.target.getAttribute("data-network"));
+			return;
+		}
+
+		if (selectedItemRef.current.contains(event.target)) {
+			if (listRef.current.style.display === "block") {
+				hideList();
+			} else {
+				showList();
+			}
+			return;
+		}
+
+		if (!_.isNil(listRef.current) && !listRef.current.contains(event.target)) {
+			hideList();
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener("click", clickListener, true);
+
+		return () => {
+			document.removeEventListener("click", clickListener);
+		};
+	});
+
 	return (
-		<select
-			className={cx("select")}
-			value={network}
-			onChange={e => {
-				setNetwork(e.target.value);
-				window.location.reload();
-			}}>
-			{Object.values(networks).map((network, index) => (
-				<option key={"network-" + index} value={network}>
-					{network}
-				</option>
-			))}
-		</select>
+		<div className={cx("network-switcher")}>
+			<div className={cx("selected-item")} ref={selectedItemRef}>
+				<input type='text' className={cx("text-field")} value={network} readOnly />
+				<DownAngleIcon className={cx("arrow")} />
+			</div>
+			<div className={cx("list")} ref={listRef}>
+				{Object.values(networks).map((item, index) => (
+					<div key={"list-item-" + index} className={cx("list-item")} data-network={item}>
+						{item}
+					</div>
+				))}
+			</div>
+		</div>
 	);
 });
 
