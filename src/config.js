ccafde707b318068e4a1568c37734550f5c4780e
chuyenbka <chuyenbka@gmail.com>
2021-06-16 14:20:13 +0700

    fix url api testnet

--- src/config.js
+++ src/config.js
@@ -5,11 +5,13 @@ const isTestnet = localStorage?.getItem("network") === networks.TESTNET;
 const config = Object.freeze({
 	// API_DEV: "https://api-binance-testnet.cosmostation.io/v1",
 	API_DEV: isTestnet
-		? process.env.REACT_APP_API_TESTNET || "https://api-binance-mainnet.cosmostation.io/v1"
+		? process.env.REACT_APP_API_TESTNET || "https://api.testnet.scan.orai.io/v1"
 		: process.env.REACT_APP_API_DEV || "https://api.scan.orai.io/v1",
-	API_PROD: process.env.REACT_APP_API_PROD || "https://api-binance-mainnet.cosmostation.io/v1",
+	API_PROD: isTestnet
+		? process.env.REACT_APP_API_TESTNET || "https://api.testnet.scan.orai.io/v1"
+		: process.env.REACT_APP_API_PROD || "https://api.scan.orai.io/v1",
 	walletapi: isTestnet
-		? process.env.REACT_APP_WALLET_API_TESTNET || "https://api-binance-mainnet.cosmostation.io/v1"
+		? process.env.REACT_APP_WALLET_API_TESTNET || "https://testnet-wallet.web.app"
 		: process.env.REACT_APP_WALLET_API || "https://api.wallet.orai.io/",
 	hasTestnetNetwork: process.env.REACT_APP_API_TESTNET && process.env.REACT_APP_WALLET_API_TESTNET,
 });
