3697e2480627af0617779e030b865cea304ab767
chuyenbka <chuyenbka@gmail.com>
2021-07-07 18:10:57 +0700

    call block info to check contain txhash

--- src/containers/Tx/useGetTx.js
+++ src/containers/Tx/useGetTx.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import {useParams, useLocation} from "react-router-dom";
+import _ from "lodash";
 import axios from "axios";
 import consts from "src/constants/consts";
 import {isTestnet} from "src/config";
@@ -25,6 +26,19 @@ const isTxFetchSuccess = txRpc => {
 	return txRpc?.data?.result?.height && parseInt(txRpc?.data?.result?.height) > 0;
 };
 
+const isBlockContainTxhash = (blockInfo, txhash) => {
+	if (!blockInfo?.data?.data || !_.isArray(blockInfo.data.data)) {
+		return false;
+	}
+	
+	for (let i = 0; i < blockInfo.data.data.length; i++) {
+		if (blockInfo.data.data[i].tx_hash === txhash) {
+			return true;
+		}
+	}
+	return false;
+};
+
 async function getDataAsync(path) {
 	try {
 		const result = await axios.get(path);
@@ -48,38 +62,67 @@ const useGetTx = txHash => {
 		const cancelToken = axios.CancelToken;
 		const source = cancelToken.source();
 		async function getTxFromRpc(count) {
-			// const txRpc = await getDataAsync(`https://rpc.orai.io/tx?hash=0x${txHash}`);
-			// if (isTxFetchSuccess(txRpc) || isTestnet) {
-			// 	const getTxScan = async () => {
-			// 		const tx = await getDataAsync(path);
-			// 		if (tx?.data?.height && parseInt(tx?.data?.height) > 0) {
-			// 			setData(tx?.data);
-			// 			setLoading(false);
-			// 			return;
-			// 		}
-			// 		setTimeout(getTxScan, 2000);
-			// 	};
-			// 	getTxScan();
-			// 	return;
-			// }
+			const txRpc = await getDataAsync(`https://rpc.orai.io/tx?hash=0x${txHash}`);
+			if (isTxFetchSuccess(txRpc) || isTestnet) {
+				const getTxScan = async () => {
+					const tx = await getDataAsync(path);
+					if (tx?.data?.height && parseInt(tx?.data?.height) > 0) {
+						setData(tx?.data);
+						setLoading(false);
+						return;
+					}
+					setTimeout(getTxScan, 2000);
+				};
+				getTxScan();
+				return;
+			}
 
-			// if (count === 4) {
-			// 	setData(null);
-			// 	setLoading(false);
-			// 	return;
-			// }
+			if (count === 4) {
+				setData(null);
+				setLoading(false);
+				return;
+			}
 
 			if (pendingTxState) {
 				setData(convertData(pendingTxState.source, txHash));
 				setLoading(false);
 			}
 
-			// setTimeout(() => {
-			// 	getTxFromRpc(count + 1);
-			// }, 2000);
+			setTimeout(() => {
+				getTxFromRpc(count + 1);
+			}, 2000);
 		}
 
-		getTxFromRpc(1);
+		async function getTxFromBlock(initHeight, height) {
+			const blockInfo = await getDataAsync( `${consts.API_BASE}${consts.API.TXS_BLOCK}/${height}?limit=1000`);
+			console.log('blockInfo === ', blockInfo)
+			if (!blockInfo?.data?.data) {
+				setTimeout(() => {
+					getTxFromBlock(initHeight, height);
+				}, 2000);
+			} else if (!isBlockContainTxhash(blockInfo, txHash) && initHeight - height < 7) {
+				setTimeout(() => {
+					getTxFromBlock(initHeight, height + 1);
+				}, 2000);
+			} else if (isBlockContainTxhash(blockInfo, txHash)) {
+				const tx = await getDataAsync(path);
+				if (tx?.data?.height && parseInt(tx?.data?.height) > 0) {
+					setData(tx?.data);
+					setLoading(false);
+					return;
+				}
+			} else {
+				setData(null);
+				setLoading(false);
+				return;
+			}
+		}
+
+		if (pendingTxState?.res?.height && parseInt(pendingTxState?.res?.height)) {
+			getTxFromBlock(parseInt(pendingTxState?.res?.height), parseInt(pendingTxState?.res?.height));
+		} else {
+			getTxFromRpc(1);
+		}
 
 		return () => {
 			source.cancel("cleanup cancel");
