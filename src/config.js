51f1c6433d39ea0f45e1c9e53700b851e50b5eb4
Pham The Huy <huypt@rikkeisoft.com>
2021-07-09 16:56:36 +0700

    Remove duplicate notification

--- src/config.js
+++ src/config.js
@@ -3,9 +3,10 @@ import {networks} from "./constants/networks";
 export const isTestnet = process.env.REACT_APP_IS_TEST_NET || localStorage?.getItem("network") === networks.TESTNET;
 
 const config = Object.freeze({
-	SCAN_API: isTestnet
-		? process.env.REACT_APP_API_TESTNET || "https://api.testnet.scan.orai.io/v1"
-		: process.env.REACT_APP_API_PROD || process.env.REACT_APP_API_DEV || "https://api.scan.orai.io/v1",
+	// SCAN_API: isTestnet
+	// 	? process.env.REACT_APP_API_TESTNET || "https://api.testnet.scan.orai.io/v1"
+	// 	: process.env.REACT_APP_API_PROD || process.env.REACT_APP_API_DEV || "https://api.scan.orai.io/v1",
+	SCAN_API: "http://localhost:5000/v1",
 	LCD_API: isTestnet ? process.env.REACT_APP_LCD_API_TESTNET || "https://lcd.testnet.orai.io" : process.env.REACT_APP_LCD_API || "https://lcd.orai.io",
 	walletapi: isTestnet
 		? process.env.REACT_APP_WALLET_API_TESTNET || "https://testnet-wallet.web.app"
