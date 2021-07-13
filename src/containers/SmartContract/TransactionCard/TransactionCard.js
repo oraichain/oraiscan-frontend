c86498097f18876bc40b5e3d2b405245ef1d934b
DUONGTK-GD\DuongTK <duongtd@rikkeisoft.com>
2021-07-13 16:12:35 +0700

    Smart contract transaction card

---
+++ src/containers/SmartContract/TransactionCard/TransactionCard.js
@@ -0,0 +1,71 @@
+import React, {memo, useState, useRef} from "react";
+import {useGet} from "restful-react";
+import {useTheme} from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import classNames from "classnames/bind";
+import consts from "src/constants/consts";
+import TransactionTable from "src/components/TxList/TransactionTable";
+import TransactionTableSkeleton from "src/components/TxList/TransactionTable/TransactionTableSkeleton";
+import TransactionCardList from "src/components/TxList/TransactionCardList";
+import TransactionCardListSkeleton from "src/components/TxList/TransactionCardList/TransactionCardListSkeleton";
+import Pagination from "src/components/common/Pagination";
+import NoResult from "src/components/common/NoResult";
+import styles from "./TransactionCard.scss";
+
+const cx = classNames.bind(styles);
+
+const TransactionCard = memo(({address = "", account = ""}) => {
+	const theme = useTheme();
+	const isLargeScreen = useMediaQuery(theme.breakpoints.up("lg"));
+	const [pageId, setPageId] = useState(1);
+	const totalPagesRef = useRef(null);
+
+	const onPageChange = page => {
+		setPageId(page);
+	};
+
+	const basePath = `${consts.API.TXS_CONTRACT}/${address}?limit=${consts.REQUEST.LIMIT}`;
+	const path = `${basePath}&page_id=${pageId}`;
+
+	const {data, loading, error} = useGet({
+		path: path,
+	});
+
+	let tableSection;
+	let paginationSection;
+
+	if (loading) {
+		tableSection = isLargeScreen ? <TransactionTableSkeleton /> : <TransactionCardListSkeleton />;
+	} else {
+		if (error) {
+			totalPagesRef.current = null;
+			tableSection = <NoResult />;
+		} else {
+			if (!isNaN(data?.page?.total_page)) {
+				totalPagesRef.current = data.page.total_page;
+			} else {
+				totalPagesRef.current = null;
+			}
+
+			if (Array.isArray(data?.data) && data.data.length > 0) {
+				tableSection = isLargeScreen ? <TransactionTable data={data.data} account={account} /> : <TransactionCardList data={data.data} account={account} />;
+			} else {
+				tableSection = <NoResult />;
+			}
+		}
+	}
+
+	paginationSection = totalPagesRef.current ? <Pagination pages={totalPagesRef.current} page={pageId} onChange={(e, page) => onPageChange(page)} /> : <></>;
+
+	return (
+		<div className={cx("transaction-card")}>
+			<div className={cx("transaction-card-header")}>Transactions</div>
+			<div className={cx("transaction-card-body")}>
+				{tableSection}
+				{paginationSection}
+			</div>
+		</div>
+	);
+});
+
+export default TransactionCard;
