0d8053a8176d283600cfbef1769ca36712cf2136
Do Nhu Thuong <thuongdo@Dos-MacBook-Pro.local>
2022-03-16 23:53:35 +0700

    Coding: ibc relayers

--- src/containers/Relayers/ChannelList.js
+++ src/containers/Relayers/ChannelList.js
@@ -1,17 +1,40 @@
-import React, {useMemo} from "react";
+import React, {useMemo, useState} from "react";
 import cn from "classnames/bind";
+import moment from "moment";
 import Row from "antd/lib/row";
 import Col from "antd/lib/col";
 import Pagination from "antd/lib/pagination";
 import Space from "antd/lib/space";
 import Avatar from "antd/lib/avatar/avatar";
 
+// constant
+import {STATUS_COLOR} from "./constants";
+
+// logo
+import OraiLogo from "src/assets/header/logo.svg";
+
 // styles
 import styles from "./Channel.module.scss";
 
 const cx = cn.bind(styles);
 
-const ChannelList = ({channels, channelName}) => {
+const dataLimit = 10;
+
+const ChannelList = ({channels, channelName, image}) => {
+	const [currentPage, setCurrentPage] = useState(1);
+
+	const onChangePage = page => {
+		setCurrentPage(page);
+	};
+
+	const getPaginatedData = () => {
+		const startIndex = currentPage * dataLimit - dataLimit;
+		const endIndex = startIndex + dataLimit;
+		return channels.slice(startIndex, endIndex);
+	};
+
+	const listChannels = getPaginatedData();
+
 	const renderHeaderChannels = useMemo(() => {
 		return (
 			<Row gutter={{xs: 8, sm: 16, md: 24, lg: 32}}>
@@ -21,8 +44,8 @@ const ChannelList = ({channels, channelName}) => {
 				<Col span={4}>
 					<div className={cx("flexCenter")}>
 						<Space>
-							<Avatar size='small' />
-							{channelName}
+							<Avatar size='small' src={<img src={OraiLogo} />} />
+							Oraichain
 						</Space>
 					</div>
 				</Col>
@@ -30,8 +53,8 @@ const ChannelList = ({channels, channelName}) => {
 				<Col span={4}>
 					<div className={cx("flexCenter")}>
 						<Space>
-							<Avatar size='small' />
-							channel partner
+							<Avatar size='small' src={image} />
+							{channelName}
 						</Space>
 					</div>
 				</Col>
@@ -53,6 +76,10 @@ const ChannelList = ({channels, channelName}) => {
 
 	const renderChannels = useMemo(() => {
 		const renderChannelItem = (item, index) => {
+			var createdAt = moment(item?.channel?.created_at, "YYYY-MM-DD");
+			const currentDate = moment().startOf("day");
+			const operatingPeriod = moment.duration(currentDate.diff(createdAt)).asDays();
+
 			return (
 				<Row gutter={{xs: 8, sm: 16, md: 24, lg: 32}} className={cx("body-list")}>
 					<Col span={1}>
@@ -60,44 +87,48 @@ const ChannelList = ({channels, channelName}) => {
 					</Col>
 					<Col span={4}>
 						<div className={cx("channel-name", "flexCenter")}>
-							<div>{item?.channel_id}</div>
-							<span className={cx("dot")} />
+							<div>{item?.channel?.channel_id}</div>
+							<span className={cx("dot")} style={{background: STATUS_COLOR[item?.channel?.status]}} />
 						</div>
 					</Col>
-					<Col span={1}>
-						<div className={cx("line-wrapper")} />
+					<Col span={1} style={{zIndex: 1}}>
+						<div className={cx("line-wrapper")} style={{background: STATUS_COLOR[item?.channel?.status]}} />
 					</Col>
 					<Col span={4}>
 						<div className={cx("channel-name", "partners")}>
-							<span className={cx("dot")} />
-							<div>{item?.counterparty_channel_id}</div>
+							<span className={cx("dot")} style={{background: STATUS_COLOR[item?.channel?.status]}} />
+							<div>{item?.channel?.counterparty_channel_id}</div>
 						</div>
 					</Col>
 
 					<Col span={4} />
 
 					<Col span={4}>
-						<div>52 Days</div>
+						<div>
+							{operatingPeriod}&nbsp;{operatingPeriod > 1 ? "Days" : "Day"}
+						</div>
 					</Col>
 					<Col span={3}>
-						<div>xxxxzzz</div>
+						<div>{item?.total_txs_within_24h}</div>
 					</Col>
 					<Col span={3}>
-						<div>xxxxx</div>
+						<div>$&nbsp;{item?.total_value_within_24h?.toFixed(2)}</div>
 					</Col>
 				</Row>
 			);
 		};
 
-		return channels.map((item, index) => renderChannelItem(item, index));
-	}, [channels]);
+		return listChannels.map((item, index) => renderChannelItem(item, index));
+	}, [listChannels]);
 
 	return (
 		<div className={cx("channel-list-wrapper")}>
 			{renderHeaderChannels}
 			{renderChannels}
 			<div className={cx("pagination")}>
-				<Pagination defaultCurrent={2} total={50} size='small' />
+				{channels?.length > 10 && (
+					<Pagination defaultCurrent={1} total={channels?.length} size='small' onChange={onChangePage} current={currentPage} pageSize={dataLimit} />
+				)}
 			</div>
 		</div>
 	);
--- src/containers/Relayers/index.js
+++ src/containers/Relayers/index.js
@@ -1,20 +1,16 @@
-import React, {useMemo, useEffect, useState} from "react";
+import React, {useMemo} from "react";
 import cn from "classnames/bind";
 import {useGet} from "restful-react";
-import InfiniteScroll from "react-infinite-scroll-component";
 import List from "antd/lib/list";
 import Avatar from "antd/lib/avatar";
-import Skeleton from "antd/lib/skeleton";
 import Collapse from "antd/lib/collapse";
-import Divider from "antd/lib/divider";
-import Tag from "antd/lib/tag";
 import Container from "@material-ui/core/Container";
 
 // components
 import ChannelList from "./ChannelList";
 
 // constants
-import {STATUS_COLOR} from "./constants";
+import {STATUS_COLOR, STATE} from "./constants";
 import consts from "src/constants/consts";
 
 // styles
@@ -24,50 +20,28 @@ const cx = cn.bind(styles);
 const {Panel} = Collapse;
 
 const Relayers = () => {
-	// const [data, setData] = useState([]);
-
-	const {data, loading, refetch} = useGet({
+	const {data} = useGet({
 		path: consts.API.IBC_RELAYERS,
 	});
 
-	const arrayKeys = data && Object.keys(data);
-
-	const lstRelayers = arrayKeys?.map(item => ({
-		...data[item],
-		name: item,
-	}));
-
-	// const loadMoreData = () => {
-	// 	if (loading) {
-	// 		return;
-	// 	}
-	// 	setLoading(true);
-	// 	fetch("https://randomuser.me/api/?results=10&inc=name,gender,email,nat,picture&noinfo")
-	// 		.then(res => res.json())
-	// 		.then(body => {
-	// 			setData([...data, ...body.results]);
-	// 			setLoading(false);
-	// 		})
-	// 		.catch(() => {
-	// 			setLoading(false);
-	// 		});
-	// };
-
-	function callback(key) {
-		console.log(key);
-	}
+	const dataSource = data && Object.values(data);
 
 	const renderList = useMemo(() => {
 		const genHeader = item => {
+			const listStatus = item?.channels?.map(i => i?.channel?.status);
+			const atLeastOpenStatus = listStatus.some(item => item != STATE.STATE_CLOSED);
+			const listOpenStatus = item?.channels?.filter(i => i?.channel?.status == STATE.STATE_OPEN);
+			const colorStatus = atLeastOpenStatus ? STATUS_COLOR[STATE.STATE_OPEN] : STATUS_COLOR[STATE.STATE_CLOSED];
+			const backgroundStatus = atLeastOpenStatus ? "rgba(55,204,110,.1)" : "rgba(255,39,69,.1)";
 			return (
 				<List.Item key={item.id}>
-					<List.Item.Meta avatar={<Avatar size='large' />} title={<a href='https://ant.design'>{item.name}</a>} description={item?.email} />
+					<List.Item.Meta avatar={<Avatar size='large' src={item?.images?.large} />} title={item.name} description={item?.denom} />
 					<div className={cx("extra-list")}>
-						<div className={cx("extra-list-tag")} style={{background: "rgba(55,204,110,.1)", color: `${STATUS_COLOR.OPENED}`}}>
-							<span className={cx("dot")} />
-							<div>Opened</div>
+						<div className={cx("extra-list-tag")} style={{background: backgroundStatus, color: colorStatus}}>
+							<span className={cx("dot")} style={{background: colorStatus}} />
+							<div>{atLeastOpenStatus ? "Opened" : "Closed"}</div>
 						</div>
-						<div className={cx("extra-list-info")}>Channel 1/1</div>
+						<div className={cx("extra-list-info")}>Channel {`${listOpenStatus?.length}/${listStatus?.length}`}</div>
 					</div>
 				</List.Item>
 			);
@@ -75,35 +49,19 @@ const Relayers = () => {
 
 		const renderItem = item => {
 			return (
-				<Collapse onChange={callback} expandIconPosition='right' className={cx("item-list")}>
+				<Collapse expandIconPosition='right' className={cx("item-list")}>
 					<Panel header={genHeader(item)} key='1'>
-						<ChannelList channels={item?.channels} channelName={item?.name} />
+						<ChannelList channels={item?.channels} channelName={item?.name} image={item?.images?.small} />
 					</Panel>
 				</Collapse>
 			);
 		};
 
-		return lstRelayers?.map(item => renderItem(item));
-	}, [lstRelayers]);
+		return dataSource?.map(item => renderItem(item));
+	}, [dataSource]);
 
 	return (
 		<Container fixed className={cx("relayers")}>
-			{/* <div
-				id='scrollableDiv'
-				style={{
-					height: "calc(100vh - 300px)",
-					overflow: "auto",
-				}}>
-				<InfiniteScroll
-					dataLength={data.length}
-					next={loadMoreData}
-					hasMore={data.length < 50}
-					loader={<Skeleton avatar paragraph={{rows: 1}} active />}
-					endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
-					scrollableTarget='scrollableDiv'>
-				</InfiniteScroll>
-				{renderList}
-			</div> */}
 			<div className={cx("page-title")}>
 				<h1>ibc relayers</h1>
 			</div>
