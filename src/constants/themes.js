1ae8ff5b692ad23c9fa6d6033a0f2b769e573186
ducphamle2 <ducphamle212@gmail.com>
2022-03-24 15:27:19 +0700

    update color theme

--- src/constants/themes.js
+++ src/constants/themes.js
@@ -368,9 +368,9 @@ export const themes = {
 						highChartLinearBorderColor_2: colors.COLUMBIA_BLUE,
 					},
 					validators: {
-						deletegateButtonBorderColor: colors.PACIFIC_BLUE,
+						deletegateButtonBorderColor: colors.MEDIUM_STATE_BLUE,
 						deletegateButtonBackgroundColor: "none",
-						deletegateButtonTextColor: colors.PACIFIC_BLUE,
+						deletegateButtonTextColor: colors.MEDIUM_STATE_BLUE,
 						cumulativeShareBarColor_1: colors.CREAM,
 						cumulativeShareBarColor_2: colors.PALE_GREEN,
 						logoBorder: colors.ROYAL_BLUE,
