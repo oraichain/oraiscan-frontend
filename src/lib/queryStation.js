1d2effddb16a0499eb1068f70f74c81805127af2
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2023-07-18 15:52:01 +0700

    cache list proposal

--- src/lib/queryStation.js
+++ src/lib/queryStation.js
@@ -2,20 +2,21 @@
 import { network } from "src/lib/config/networks";
 import { QueryClient, setupGovExtension } from "@cosmjs/stargate";
 import { Tendermint34Client } from "@cosmjs/tendermint-rpc";
-
-export const broadcastModeObj = {
-	BROADCAST_MODE_BLOCK: "BROADCAST_MODE_BLOCK",
-	BROADCAST_MODE_ASYNC: "BROADCAST_MODE_ASYNC",
-	BROADCAST_MODE_SYNC: "BROADCAST_MODE_SYNC",
-	BROADCAST_MODE_UNSPECIFIED: "BROADCAST_MODE_UNSPECIFIED",
-};
+import * as cosmwasm from "@cosmjs/cosmwasm-stargate";
 
 export default class QueryStation {
 	constructor() {}
 	queryClientTendermint = async () => {
 		const tendermint = await Tendermint34Client.connect(network.rpc);
+		// const tendermint = await Tendermint34Client.connect("http://3.134.19.98:26657");
 		const queryClient = QueryClient.withExtensions(tendermint, setupGovExtension);
 		return queryClient;
+	};
+
+	queryClient = async () => {
+		const client = await cosmwasm.CosmWasmClient.connect(network.rpc);
+		// const client = await cosmwasm.CosmWasmClient.connect("http://3.134.19.98:26657");
+		return client;
 	};
 
 	proposalId = async proposalId => {
@@ -28,12 +29,52 @@ export default class QueryStation {
 		}
 	};
 
-	proposalList = async (proposalStatus, depositor = "", voter = "", paginationKey) => {
+	deposits = async (proposalId, paginationKey = undefined) => {
 		try {
 			const queryClient = await this.queryClientTendermint();
-			return await queryClient.gov.proposals(proposalStatus, depositor, voter);
+			return await queryClient.gov.deposits(proposalId, paginationKey);
 		} catch (ex) {
 			console.log("proposalId msg error: ", ex);
+			throw ex;
+		}
+	};
+
+	tally = async proposalId => {
+		try {
+			const queryClient = await this.queryClientTendermint();
+			return await queryClient.gov.tally(proposalId);
+		} catch (ex) {
+			console.log("proposalId msg error: ", ex);
+			throw ex;
+		}
+	};
+
+	proposalList = async (proposalStatus, depositor = "", voter = "", paginationKey = undefined, offset, limit) => {
+		try {
+			const queryClient = await this.queryClientTendermint();
+			return await queryClient.gov.proposals(proposalStatus, depositor, voter, paginationKey, offset, limit);
+		} catch (ex) {
+			console.log("proposalId msg error: ", ex);
+			throw ex;
+		}
+	};
+
+	votes = async (proposalId, paginationKey = undefined) => {
+		try {
+			const queryClient = await this.queryClientTendermint();
+			return await queryClient.gov.votes(proposalId, paginationKey);
+		} catch (ex) {
+			console.log("proposalId msg error: ", ex);
+			throw ex;
+		}
+	};
+
+	searchTx = async queryTx => {
+		try {
+			const client = await this.queryClient();
+			return await client.searchTx(queryTx);
+		} catch (ex) {
+			console.log("searchTx msg error: ", ex);
 			throw ex;
 		}
 	};
