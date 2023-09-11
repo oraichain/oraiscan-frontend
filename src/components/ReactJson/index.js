2d9dbfe3d9097754e8d0d436eaa571ceee1823d3
Pham Tu <tubackkhoa@gmail.com>
2023-09-11 19:39:38 +0700

    change style

--- src/components/ReactJson/index.js
+++ src/components/ReactJson/index.js
@@ -1,26 +1,34 @@
 import React from "react";
 import { JSONTree } from "react-json-tree";
+import cn from "classnames/bind";
+import styles from "./ReactJson.module.scss";
+
+const cx = cn.bind(styles);
 
 const ReactJson = ({ style, name, theme, displayObjectSize, collapsed, displayDataTypes, src, sortKeys, quotesOnKeys }) => {
 	return (
-		<JSONTree
-			data={src}
-			theme={theme}
-			shouldExpandNodeInitially={(keyPath, data, level) => {
-				return collapsed && level < collapsed;
-			}}
-			sortObjectKeys={sortKeys}
-			valueRenderer={(_, raw) => {
-				if (typeof raw === "string" && raw.match(/^https?:\/\//)) {
-					return (
-						<a target='_blank' href={raw}>
-							{raw}
-						</a>
-					);
-				}
-				return raw;
-			}}
-		/>
+		<div className={cx("json-viewer")} style={style}>
+			<JSONTree
+				data={src}
+				hideRoot
+				theme={theme}
+				collectionLimit={displayObjectSize}
+				shouldExpandNodeInitially={(keyPath, data, level) => {
+					return !collapsed || level < collapsed;
+				}}
+				sortObjectKeys={sortKeys}
+				valueRenderer={(_, raw) => {
+					if (typeof raw === "string" && raw.match(/^https?:\/\//)) {
+						return (
+							<a target='_blank' href={raw}>
+								{raw}
+							</a>
+						);
+					}
+					return raw;
+				}}
+			/>
+		</div>
 	);
 };
 
