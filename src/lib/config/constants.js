e59ba3a90fd7f6b6eeb8e2df7c23b6d5a893a793
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2023-10-12 09:55:33 +0700

    hide txs contract meme fe

--- src/lib/config/constants.js
+++ src/lib/config/constants.js
@@ -1,15 +1,15 @@
 //new const
 
-export const CHROME = 'https://google.com/chrome';
+export const CHROME = "https://google.com/chrome";
 
 /* oraiswap:unit */
-export const ORAI = 'orai';
-export const UAIRI = 'uAIRI';
-export const AIRI = 'AIRI';
-export const ATOM = 'ATOM';
-export const LP = 'LP';
-export const BEP20_ORAI = 'bep20_orai';
-export const ERC20_ORAI = 'erc20_orai';
+export const ORAI = "orai";
+export const UAIRI = "uAIRI";
+export const AIRI = "AIRI";
+export const ATOM = "ATOM";
+export const LP = "LP";
+export const BEP20_ORAI = "bep20_orai";
+export const ERC20_ORAI = "erc20_orai";
 
 export const NATIVE_TOKENS = [ORAI];
 export const DECIMAL_FRACTION = Math.pow(10, 18);
@@ -24,19 +24,20 @@ export const AXIOS_THROTTLE_THRESHOLD = 2000;
 export const AXIOS_TIMEOUT = 10000;
 
 /* project */
-export const MEDIUM = '';
-export const DISCORD = '';
-export const TELEGRAM = '';
-export const WECHAT = '';
-export const GITHUB = 'https://github.com/oraichain/oraiscan-frontend';
+export const MEDIUM = "";
+export const DISCORD = "";
+export const TELEGRAM = "";
+export const WECHAT = "";
+export const GITHUB = "https://github.com/oraichain/oraiscan-frontend";
 
 // bsc and eth information
-export const ETHEREUM_CHAIN_ID = '0x1';
-export const ETHEREUM_RPC =
-  'https://mainnet.infura.io/v3/648e041e75924b5c9d0254e4a76c9978';
-export const BSC_CHAIN_ID = '0x38';
-export const BSC_RPC = 'https://bsc-dataseed.binance.org';
+export const ETHEREUM_CHAIN_ID = "0x1";
+export const ETHEREUM_RPC = "https://mainnet.infura.io/v3/648e041e75924b5c9d0254e4a76c9978";
+export const BSC_CHAIN_ID = "0x38";
+export const BSC_RPC = "https://bsc-dataseed.binance.org";
 
-export const ORAI_ETH_CONTRACT = '0x4c11249814f11b9346808179cf06e71ac328c1b5';
-export const ORAI_BSC_CONTRACT = '0xA325Ad6D9c92B55A3Fc5aD7e412B1518F96441C0';
-export const AIRI_BSC_CONTRACT = '0x7e2a35c746f2f7c240b664f1da4dd100141ae71f';
+export const ORAI_ETH_CONTRACT = "0x4c11249814f11b9346808179cf06e71ac328c1b5";
+export const ORAI_BSC_CONTRACT = "0xA325Ad6D9c92B55A3Fc5aD7e412B1518F96441C0";
+export const AIRI_BSC_CONTRACT = "0x7e2a35c746f2f7c240b664f1da4dd100141ae71f";
+
+export const SMARTCONTRACT_IGNORE = "orai1kvu7xclv2uvc5yl0mzgcux0cw40sjur2kksarva84376gq4qnnxqhk2hh5";
