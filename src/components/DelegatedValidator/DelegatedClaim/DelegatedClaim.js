a3306ac0dd1445aa1757ff5f37ce80c229379f57
ledanghuy1811 <huygamo567@gmail.com>
2024-08-14 15:43:07 +0700

    fix: delegator-validator UI when have small rewards

--- src/components/DelegatedValidator/DelegatedClaim/DelegatedClaim.js
+++ src/components/DelegatedValidator/DelegatedClaim/DelegatedClaim.js
@@ -1,8 +1,8 @@
 // @ts-nocheck
 import React from "react";
-import {useGet} from "restful-react";
+import { useGet } from "restful-react";
 import cn from "classnames/bind";
-import {useTheme} from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import consts from "src/constants/consts";
 import ClaimTable from "src/components/DelegatedValidator/DelegatedClaim/ClaimTable/ClaimTable";
@@ -12,25 +12,31 @@ import ClaimCardListSkeleton from "src/components/DelegatedValidator/DelegatedCl
 import NoResult from "src/components/common/NoResult";
 import arrowIcon from "src/assets/wallet/arrow.svg";
 import styles from "./DelegatedClaim.module.scss";
+import { formatOrai } from "src/helpers/helper";
 
 const cx = cn.bind(styles);
 
-export default function({setActiveTab, address}) {
+export default function({ setActiveTab, address }) {
 	const theme = useTheme();
 	const isLargeScreen = useMediaQuery(theme.breakpoints.up("lg"));
 	const path = consts.API.WALLET.CLAIM_REWARD + "/" + address;
-	const {data} = useGet({
+	const { data } = useGet({
 		path: path,
 	});
 
 	let tableSection;
 
 	if (data) {
-		if (Array.isArray(data?.claim_reward) && data.claim_reward.length > 0) {
+		const claimData =
+			Array.isArray(data?.claim_reward) && data.claim_reward.length > 0
+				? data?.claim_reward.filter(claim => parseInt(claim.staked) > 0 || parseFloat(formatOrai(claim.claimable_rewards)) >= parseFloat(formatOrai(1)))
+				: [];
+
+		if (claimData.length > 0) {
 			tableSection = isLargeScreen ? (
-				<ClaimTable data={data.claim_reward} totalStaked={data?.total_staked} totalRewards={data?.total_rewards} />
+				<ClaimTable data={claimData} totalStaked={data?.total_staked} totalRewards={data?.total_rewards} />
 			) : (
-				<ClaimCardList data={data.claim_reward} totalStaked={data?.total_staked} totalRewards={data?.total_rewards} />
+				<ClaimCardList data={claimData} totalStaked={data?.total_staked} totalRewards={data?.total_rewards} />
 			);
 		} else {
 			tableSection = <NoResult />;
