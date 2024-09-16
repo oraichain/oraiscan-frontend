c764c17a18837907bcdc3161d3655a9de26e21b0
QuanPT <quan.pt@orai.io>
2024-09-16 19:39:17 +0700

    feat(smart contract): add function copy address

--- src/components/SmartContract/ContractPreview.js
+++ src/components/SmartContract/ContractPreview.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
 import cn from "classnames/bind";
 import { isNil } from "lodash-es";
 import Grid from "@material-ui/core/Grid";
-import Skeleton from "@material-ui/lab/Skeleton";
-import InfoRow from "src/components/common/InfoRow";
-import styles from "./ContractPreview.module.scss";
-import { NavLink } from "react-router-dom";
-import consts from "src/constants/consts";
 import Tooltip from '@material-ui/core/Tooltip';
 import { makeStyles } from '@material-ui/core/styles';
+import Skeleton from "@material-ui/lab/Skeleton";
+import copy from "copy-to-clipboard";
+
+import InfoRow from "src/components/common/InfoRow";
+import consts from "src/constants/consts";
+import { ReactComponent as CopyIcon } from 'src/assets/icons/copy.svg';
+import { ReactComponent as CheckIcon } from 'src/assets/icons/check.svg';
+import styles from "./ContractPreview.module.scss";
 
 const cx = cn.bind(styles);
 
@@ -18,9 +22,10 @@ const useStyles = makeStyles((theme) => ({
 	  maxWidth: 600,
 	  fontSize: 16,
 	},
-  }));
+}));
 
 const HeaderCardSkeleton = ({ data }) => {
+	const [copied, setCopied] = React.useState(false);
 	const classes = useStyles();
 	return (
 		<Grid item lg={6} xs={12}>
@@ -38,10 +43,29 @@ const HeaderCardSkeleton = ({ data }) => {
 							<td>
 								<div className={cx("item-title")}>Address</div>
 							</td>
-							<td>
+							<td className={cx('contract-address')}>
 								<Tooltip title={data?.address} classes={{ tooltip: classes.customWidth }} className={cx("item-text")}>
 									<div >{isNil(data?.address) ? "-" : data?.address}</div>
 								</Tooltip>
+
+								{
+									data?.address ? (
+										copied ? (
+											<Tooltip title="Copied" classes={{ tooltip: classes.customWidth }}>
+												<CheckIcon />
+											</Tooltip>
+										) : (
+											<Tooltip title="Copy Address" classes={{ tooltip: classes.customWidth }} enterTouchDelay={0}>
+												<CopyIcon className={cx('copy-btn')} onClick={() => {
+													copy(data?.address);
+													setCopied(true);
+													setTimeout(() => setCopied(false), 1500);
+												}} />
+											</Tooltip>
+										)
+									) : (
+										''
+									)}
 							</td>
 						</tr>
 
